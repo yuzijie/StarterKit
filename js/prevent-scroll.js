a7dd279784706a4d36ad7896fef022af4c75f869
Zac YU <git@yuz.me>
2015-07-20 14:32:17 +0200

    prevent parent element from scrolling when scroll child element

---
+++ js/prevent-scroll.js
@@ -0,0 +1,18 @@
+// prevent parent element from scrolling when scroll child element
+// require jQuery Mouse Wheel Plugin
+
+require("../node_modules/jquery-mousewheel/jquery.mousewheel.js")($);
+var windowScroll = require("./window-scroll.js");
+
+var PreventScroll = function ($target) {
+    $target.on("mousewheel", function (e) {
+        if (($target.scrollTop() === $target[0].scrollHeight - $target.outerHeight() && e.deltaY < 0) ||
+            ($target.scrollTop() === 0 && e.deltaY > 0)) {
+            windowScroll.disable();
+        } else {
+            windowScroll.enable();
+        }
+    });
+};
+
+module.exports = PreventScroll;

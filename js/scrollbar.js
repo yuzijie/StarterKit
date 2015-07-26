6d748e0466a2e8428f25118dc06051cad05a57a6
Zac YU <git@yuz.me>
2015-07-27 01:08:57 +0200

    update

--- js/scrollbar.js
+++ js/scrollbar.js
@@ -1,18 +1,3 @@
-var $body = $(document.body);
-
-// From bootstrap
-// Check if scrollbar is appearing now
-var hasScrollbar = function () {
-    var fullWindowWidth = window.innerWidth;
-
-    if (!fullWindowWidth) { // workaround for missing window.innerWidth in IE8
-        var documentElementRect = document.documentElement.getBoundingClientRect();
-        fullWindowWidth = documentElementRect.right - Math.abs(documentElementRect.left);
-    }
-
-    return document.body.clientWidth < fullWindowWidth;
-};
-
 var getScrollbarWidth = function () {
     var scrollDiv = document.createElement('div');
     scrollDiv.className = "scrollbar-measure";
@@ -22,12 +7,15 @@ var getScrollbarWidth = function () {
     return scrollbarWidth;
 };
 
+var $body = $(document.body);
 var scrollbarWidth = getScrollbarWidth();
 var bodyPad = parseInt(($body.css('padding-right') || 0), 10);
 var originalBodyPad = document.body.style.paddingRight || '';
 
 var setPadding = function () {
-    if (hasScrollbar()) $body.css('padding-right', bodyPad + scrollbarWidth);
+    if (document.body.clientWidth < window.innerWidth) {
+        $body.css('padding-right', bodyPad + scrollbarWidth);
+    }
 };
 
 var resetPadding = function () {

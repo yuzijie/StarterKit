94f0946c283f9e31b70b1de4bb318b9ac34fa0ee
Zac YU <git@yuz.me>
2015-07-26 13:42:31 +0200

    upadte

--- js/float-box.js
+++ js/float-box.js
@@ -2,29 +2,30 @@ var preventScroll = require("./prevent-scroll");
 
 var FloatBox = function (box, options) {
     // Object
-    this.$box = typeof box === "string" ? $(box) : box;
+    this.self = (box instanceof jQuery) ? box : $(box);
+    options = options || {};
 
     // Options
     this.opts = $.extend({
         closeOnScroll: false,                 //   Close box on page scrolling
-        closeOnClick: true,                   //   Close box when clicking outside of it
+        closeOnClick: false,                  //   Close box when clicking outside of it
         closeOnLeave: false,                  //   Close box after mouse left box area
         preventClose: [],                     //   Prevent menu close when clicking these elements
-        preventScroll: this.$box.children()   //   Elements that need background scroll prevention
+        preventScroll: this.self.children()   //   Elements that need background scroll prevention
     }, options);
 
     // preventClose Array
     if (options.preventClose && options.preventClose.constructor !== Array) {
-        this.opts.preventClose = [this.$box, options.preventClose];
+        this.opts.preventClose = [this.self, options.preventClose];
     } else {
-        this.opts.preventClose.unshift(this.$box);
+        this.opts.preventClose.unshift(this.self);
     }
 
     // preventScroll
     if (options.preventScroll && typeof options.preventScroll === "string") {
-        this.opts.preventScroll = this.$box.find(options.preventScroll);
+        this.opts.preventScroll = this.self.find(options.preventScroll);
     }
-    if (this.opts.preventScroll) preventScroll(this.opts.preventScroll);
+    if (this.opts.preventScroll.length > 0) preventScroll(this.opts.preventScroll);
 
     // Custom actions
     this.boxOpenAction = null;
@@ -35,53 +36,51 @@ var FloatBox = function (box, options) {
 };
 
 FloatBox.prototype.setListener = function () {
-    var self = this;
+    var that = this;
 
     //Close menu on page scrolling
-    if (self.opts.closeOnScroll === true) {
+    if (that.opts.closeOnScroll === true) {
         $(window).on("scroll", function () {
-            self.close();
+            that.close();
         });
     }
 
     // Close box when clicking outside of it
-    if (self.opts.closeOnClick === true) {
+    if (that.opts.closeOnClick === true) {
         $(document).on("click", function (e) {
             var hide = true, i;
-            for (i = 0; i < self.opts.preventClose.length; i++) {
-                var $element = self.opts.preventClose[i];
+            for (i = 0; i < that.opts.preventClose.length; i++) {
+                var $element = that.opts.preventClose[i];
                 if ($element.is(e.target) || $element.has(e.target).length > 0) hide = false;
             }
-            if (hide === true) self.close();
+            if (hide === true) that.close();
         });
     }
 
     // Close box after mouse left the box area
-    if (self.opts.closeOnLeave === true) {
-        self.$box.on("mouseleave", function () {
-            self.close();
+    if (that.opts.closeOnLeave === true) {
+        that.self.on("mouseleave", function () {
+            that.close();
         });
     }
 };
 
 FloatBox.prototype.open = function () {
-    if (this.$box.is(":hidden")) {
-        this.$box.show();
+    if (this.self.is(":hidden")) {
+        this.self.show();
         if (this.boxOpenAction) this.boxOpenAction();
         return true;
-    } else {
-        return false;
     }
+    return false;
 };
 
 FloatBox.prototype.close = function () {
-    if (this.$box.is(":visible")) {
-        this.$box.hide();
+    if (this.self.is(":visible")) {
+        this.self.hide();
         if (this.boxCloseAction) this.boxCloseAction();
         return true;
-    } else {
-        return false;
     }
+    return false;
 };
 
 FloatBox.prototype.toggle = function () {
@@ -96,6 +95,13 @@ FloatBox.prototype.onOpen = function (func) {
 FloatBox.prototype.onClose = function (func) {
     if ($.isFunction(func)) this.boxCloseAction = func;
     return this;
+};
+
+FloatBox.prototype.attachTo = function (target) {
+    if (target) {
+        target = (target instanceof jQuery) ? target : $(target);
+        target.html(this.self);
+    }
 };
 
 module.exports = FloatBox;

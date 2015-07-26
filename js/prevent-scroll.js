c93047ce92e95dde2c2e4ab43f63c946fb49aaf3
Zac YU <git@yuz.me>
2015-07-26 23:00:58 +0200

    update

--- js/prevent-scroll.js
+++ js/prevent-scroll.js
@@ -2,6 +2,7 @@ require("../node_modules/jquery-mousewheel/jquery.mousewheel.js")($);
 var PreventScroll = function ($target) {
     $target = ($target instanceof jQuery) ? $target : $($target);
     $target.on("mousewheel", function (e) {
+        //console.log("scrollTop: " + $target.scrollTop() + " scrollHeight: " + $target[0].scrollHeight + " outerHeight: " + $target.outerHeight());
         e.stopPropagation();
         if (($target.scrollTop() >= $target[0].scrollHeight - $target.outerHeight() && e.deltaY < 0) ||
             ($target.scrollTop() <= 0 && e.deltaY > 0)) {

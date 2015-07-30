f60c791b958cdc5d9c87f1dd053851162cfac2d3
Zac YU <git@yuz.me>
2015-07-30 16:43:54 +0200

    update insert.js

--- js/insert.js
+++ js/insert.js
@@ -3,22 +3,26 @@ function to$(item) {
     return (item instanceof jQuery) ? item : $(item);
 }
 
-var Insert = function (template, target) {
+var Insert = function (template, target, options) {
     this.$target = to$(target); // target to insert
     this.template = template; // template file
     this.$element = null;
+
+    // Options
+    this.opts = $.extend({
+        insertMethod: "append"
+    }, options || {});
 
     // Actions
     this.insertAction = null;
     this.destroyAction = null;
 };
 
-Insert.prototype.insert = function (data, insertMethod) {
+Insert.prototype.insert = function (data) {
     data = data || {};
-    insertMethod = insertMethod || "append";
     this.$element = $(this.template(data));
     if (this.insertAction) this.insertAction(this.$element);
-    this.$target[insertMethod](this.$element);
+    this.$target[this.opts.insertMethod](this.$element);
     return this;
 };
 
@@ -28,18 +32,18 @@ Insert.prototype.destroy = function () {
     return this;
 };
 
+Insert.prototype.changeTarget = function (target) {
+    this.$target = to$(target); // target to insert
+    return this;
+};
+
 Insert.prototype.onInsert = function (func) {
     if ($.isFunction(func)) this.insertAction = func;
     return this;
 };
 
-Insert.prototype.onDestroy = function () {
+Insert.prototype.onDestroy = function (func) {
     if ($.isFunction(func)) this.destroyAction = func;
-    return this;
-};
-
-Insert.prototype.changeTarget = function (target) {
-    this.$target = to$(target); // target to insert
     return this;
 };
 

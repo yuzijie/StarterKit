67a778f3a1051813b06edf588278905e02d16472
Zac YU <git@yuz.me>
2015-07-30 12:46:18 +0200

    add name space

--- js/prevent-scroll.js
+++ js/prevent-scroll.js
@@ -1,7 +1,7 @@
 require("../node_modules/jquery-mousewheel/jquery.mousewheel.js")($);
 var PreventScroll = function ($target) {
     $target = ($target instanceof jQuery) ? $target : $($target);
-    $target.on("mousewheel", function (e) {
+    $target.on("mousewheel.preventScroll", function (e) {
         //console.log("scrollTop: " + $target.scrollTop() + " scrollHeight: " + $target[0].scrollHeight + " outerHeight: " + $target.outerHeight());
         e.stopPropagation();
         if (($target.scrollTop() >= $target[0].scrollHeight - $target.outerHeight() && e.deltaY < 0) ||

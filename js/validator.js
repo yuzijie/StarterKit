1034843f2d38fd7f133d464d4445573f2247945b
Zac YU <git@yuz.me>
2015-08-03 15:42:05 +0200

    update validator

--- js/validator.js
+++ js/validator.js
@@ -3,30 +3,55 @@ function to$(item) {
     return (item instanceof jQuery) ? item : $(item);
 }
 
+// output
+var errorObject = function (type, $element, msg) {
+    // Example:
+    // {error: "required", msg: "该项目为必填", element: "jQuery Object"}
+
+    var customMsg = $element.prop("title");
+    if (customMsg) msg = customMsg;
+
+    return {
+        error: type,
+        element: $element,
+        msg: msg
+    };
+};
+
 // Check required field
-var checkRequire = function ($input, content) {
+var checkInputRequire = function ($input, content) {
+    if (content.length > 0) return null;
+    return errorObject("required", $input, "该项目为必填");
+};
+
+var checkGroup = function ($field) {
+    if ($field.data("required") === true) {
+        if ($field.has(":checked").length === 0) return errorObject("required", $field, "该项目为必填")
+    }
+    return null;
+};
+
+var checkInput = function ($input) {
+    var content = $input.val().trim(),
+        validationError;
+
     if ($input.prop("required")) {
-        if (content.trim().length > 0) {
-            return null;
-        } else {
-            return "required";
-        }
+        validationError = checkInputRequire($input, content);
+        if (validationError) return validationError;
     }
     return null;
 };
 
 var Validator = {};
 
-Validator.check = function (input) {
-    var validationError,
-        $input = to$(input),
-        content = $input.val();
+Validator.check = function (field) {
+    var $field = to$(field);
 
-    if (validationError = checkRequire($input, content)) {
-        return validationError;
+    if ($field.is("[data-checkbox-group],[data-radio-group]")) {
+        return checkGroup($field);
+    } else {
+        return checkInput($field);
     }
-
-    return null;
 };
 
 module.exports = Validator;

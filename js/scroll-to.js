a3f37c0bb9fa1c48e417620aedd3399c8ad43b6e
Zac YU <git@yuz.me>
2015-08-03 12:02:54 +0200

    update scroll-to

--- js/scroll-to.js
+++ js/scroll-to.js
@@ -3,13 +3,24 @@ function to$(item) {
     return (item instanceof jQuery) ? item : $(item);
 }
 
-var scrollTo = function (target, container) {
+var scrollTo = function (target, container, options) {
     target = to$(target);
-    container = (container) ? to$(container) : $(window);
+    options = options || {};
 
-    container.scrollTop(
-        target.offset().top - container.offset().top + container.scrollTop()
-    );
+    if (container) {
+        container = to$(container);
+        container.stop().animate({
+            scrollTop: target.offset().top - container.offset().top + container.scrollTop()
+        }, options.duration || '1000', function () {
+            if (options["onFinish"]) options["onFinish"]();
+        });
+    } else {
+        $(document.body).stop().animate({
+            scrollTop: target.offset().top
+        }, options.duration || '1000', function () {
+            if (options["onFinish"]) options["onFinish"]();
+        });
+    }
 };
 
 module.exports = scrollTo;

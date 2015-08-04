dfbbfc74f62dfa9d496231a50f2c99c2b0295ab8
Zac YU <git@yuz.me>
2015-08-04 15:01:31 +0200

    update

--- js/insert.js
+++ js/insert.js
@@ -3,15 +3,11 @@ function to$(item) {
     return (item instanceof jQuery) ? item : $(item);
 }
 
-var Insert = function (template, target, options) {
-    this.$target = to$(target); // target to insert
-    this.template = template; // template file
+var Insert = function (template, target, insertMethod) {
+    this.$target = to$(target);                   // target to insert
+    this.template = template;                     // template file
+    this.insertMethod = insertMethod || "append"; // insert method
     this.$elements = [];
-
-    // Options
-    this.opts = $.extend({
-        insertMethod: "append"
-    }, options || {});
 
     // Actions
     this.insertAction = null;
@@ -21,16 +17,24 @@ var Insert = function (template, target, options) {
 Insert.prototype.insert = function (data) {
     data = data || {};
     var $element = $(this.template(data));
-    this.$target[this.opts.insertMethod]($element);
-    if (this.insertAction) this.insertAction($element);
+
+    // add to DOM
+    this.$target[this.insertMethod]($element);
     this.$elements.push($element);
+
+    // custom action
+    if (this.insertAction) this.insertAction($element);
     return this;
 };
 
 Insert.prototype.destroy = function () {
     if (this.$elements.length > 0) {
         var $element = this.$elements.pop();
+
+        // custom action
         if (this.destroyAction) this.destroyAction($element);
+
+        // remove from DOM
         $element.remove();
     }
     return this;
@@ -38,7 +42,11 @@ Insert.prototype.destroy = function () {
 
 Insert.prototype.destroyAll = function () {
     if (this.$elements.length > 0) {
+
+        // custom action
         if (this.destroyAction) this.destroyAction(this.$elements);
+
+        // remove all
         $.each(this.$elements, function (index, $element) {
             $element.remove();
         });

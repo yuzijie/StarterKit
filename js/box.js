f3d4f1c7a6a74a1dc4b7c39fc45e76973822633b
Zac YU <git@yuz.me>
2015-08-18 17:32:19 +0200

    bug fix

--- js/box.js
+++ js/box.js
@@ -16,7 +16,7 @@ function closeOnClick($box, id) {
 // close box when page is scrolling
 function closeOnScroll($box, id) {
     $(window).on("scroll." + id, function () {
-        $box.trigger("close", {animation: false});
+        $box.trigger("close", false);
     });
 }
 
@@ -37,37 +37,47 @@ module.exports.on = function (box, options) {
     // open action
     function open() {
         if ($box.is(":hidden")) {
-            $box.addClass("box--open").show();
-            setTimeout(function () {
-                if (opts.closeOnClick === true) closeOnClick($box, id);
-                if (opts.closeOnScroll === true) closeOnScroll($box, id);
-            }, 50);
+            var allow;
+            if (opts["beforeOpen"]) allow = opts["beforeOpen"]();
+
+            if (allow !== false) {
+                $box.addClass("box--open").show();
+                setTimeout(function () {
+                    if (opts.closeOnClick === true) closeOnClick($box, id);
+                    if (opts.closeOnScroll === true) closeOnScroll($box, id);
+                }, 50);
+                if (opts["afterOpen"]) opts["afterOpen"]();
+            }
             return true;
         }
         return false;
     }
 
     // close action
-    function close(opts) {
+    function close(animation) {
         if ($box.is(":visible") && preventClose === false) {
-            opts = opts || {};
+            var allow, duration;
+            if (opts["beforeClose"]) allow = opts["beforeClose"]();
 
-            // close the box
-            $box.removeClass("box--open").addClass("box--close");
-            $box.one(animDetect.animationEnd, function () {
-                $box.removeClass("box--close").hide();
-            });
+            if (allow !== false) {
+                // close the box
+                $box.removeClass("box--open").addClass("box--close");
+                $box.one(animDetect.animationEnd, function () {
+                    $box.removeClass("box--close").hide();
+                });
 
-            // animation is not supported or turned off manually
-            if (!animDetect.animationSupport || opts.animation === false) {
-                $box.trigger(animDetect.animationEnd);
-            } else { // no animation
-                var duration = $box.css("animation-duration").slice(0, -1) * 1000;
-                if (duration === 0) $box.trigger(animDetect.animationEnd);
+                // animation is not supported or turned off manually
+                if (!animDetect.animationSupport || animation === false) {
+                    $box.trigger(animDetect.animationEnd);
+                } else { // no animation
+                    duration = $box.css("animation-duration").slice(0, -1) * 1000;
+                    if (duration === 0) $box.trigger(animDetect.animationEnd);
+                }
+
+                // reset listeners
+                $(window).off("." + id);
             }
-
-            // reset listeners
-            $(window).off("." + id);
+            prevent(duration); // prevent close
         }
     }
 
@@ -75,13 +85,10 @@ module.exports.on = function (box, options) {
     $box.on({
         open: function () {
             open();
-            preventClose = true;
-            setTimeout(function () {
-                preventClose = false;
-            }, 50);
+            prevent(); // prevent close
         },
-        close: function (event, opts) {
-            close(opts);
+        close: function (event, animation) {
+            close(animation);
         },
         toggle: function () {
             if (!open()) close();
@@ -97,6 +104,14 @@ module.exports.on = function (box, options) {
     $box.on("click", "[data-box-close]", function () {
         $box.trigger("close");
     });
+
+    // helper functions
+    function prevent(t) { // prevent close
+        preventClose = true;
+        setTimeout(function () {
+            preventClose = false;
+        }, t || 100);
+    }
 };
 
 module.exports.getId = function (box) {

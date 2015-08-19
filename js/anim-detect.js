9a9c214bc857f5f082dfbac3b739a7b5e2381704
Zac YU <git@yuz.me>
2015-08-19 17:02:24 +0200

    add animation finish helper

--- js/anim-detect.js
+++ js/anim-detect.js
@@ -12,4 +12,23 @@ var supportTest = {
     animationEnd: ias ? "animationend" : "webkitAnimationEnd"
 };
 
+supportTest.animFinish = function (enable, $el, func) {
+    if (!$.isFunction(func)) throw "animation finish function error!";
+
+    var duration = null;
+
+    // listener
+    $el.one(supportTest.animationEnd, func);
+
+    // animation is not supported or turned off manually
+    if (!supportTest.animationSupport || enable === false) {
+        $el.trigger(supportTest.animationEnd);
+    } else { // no animation
+        duration = $el.css("animation-duration").slice(0, -1) * 1000;
+        if (duration === 0) $el.trigger(supportTest.animationEnd);
+    }
+
+    return duration;
+};
+
 module.exports = supportTest;

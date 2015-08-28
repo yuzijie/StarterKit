1a71d58ee33f6e3302e597d36c082e7608e84c22
Zac YU <git@yuz.me>
2015-08-28 19:51:36 +0200

    update

--- js/anim-detect.js
+++ js/anim-detect.js
@@ -3,16 +3,19 @@ function isSupported(property) {
 }
 
 var ias = isSupported("animation");
+var its = isSupported("transform");
 
 var supportTest = {
     transitionSupport: isSupported("transition"),
     animationSupport: ias || isSupported("webkitAnimation"),
-    animationStart: ias ? "animationstart" : "webkitAnimationStart",
-    animationIteration: ias ? "animationiteration" : "webkitAnimationIteration",
-    animationEnd: ias ? "animationend" : "webkitAnimationEnd"
+    transformSupport: its || isSupported("webkitTransform"),
+    transform: its ? "transform" : "-webkit-transform", // css transform style
+    animationStart: ias ? "animationstart" : "webkitAnimationStart", // javascript animation events
+    animationIteration: ias ? "animationiteration" : "webkitAnimationIteration", // javascript animation events
+    animationEnd: ias ? "animationend" : "webkitAnimationEnd" // javascript animation events
 };
 
-supportTest.animFinish = function (enable, $el, func) {
+supportTest.animFinish = function (enable, $el, func) { // after animation finish
     if (!$.isFunction(func)) throw "animation finish function error!";
 
     var duration = null;
@@ -26,6 +29,25 @@ supportTest.animFinish = function (enable, $el, func) {
     } else { // no animation
         duration = $el.css("animation-duration").slice(0, -1) * 1000;
         if (duration === 0) $el.trigger(supportTest.animationEnd);
+    }
+
+    return duration;
+};
+
+supportTest.tranFinish = function (enable, $el, func) { // after transform finish
+    if (!$.isFunction(func)) throw "transition finish function error!";
+
+    var duration = null;
+
+    // listener
+    $el.one("transitionend", func);
+
+    // transition or transform is not supported or turned off manually
+    if (!supportTest.transitionSupport || !supportTest.transformSupport || enable === false) {
+        $el.trigger("transitionend");
+    } else { // no transition
+        duration = $el.css("transition-duration").slice(0, -1) * 1000;
+        if (duration === 0) $el.trigger("transitionend");
     }
 
     return duration;
--- js/slider.js
+++ js/slider.js
@@ -1,8 +1,5 @@
 var h = require("./helper"),
-    template = '<div class="slider-stage"></div>',
-//transition = "left 0.6s ease";
-    transition = "all 0.6s ease";
-
+    anim = require("./anim-detect");
 
 ///////////// Helper /////////////
 function toObject(slides) {
@@ -26,21 +23,17 @@ function toObject(slides) {
     return output;
 }
 
-function getWidth(elements) {
-    var width = 0;
-    elements.each(function () {
-        width += this.offsetWidth;
-    });
-    return width;
+function getWidth(element) {
+    return element.outerWidth();
 }
 
 ////////////// Main //////////////
 module.exports.transform = function (container, slides, options) {
     if (!container) throw "invalid slider container!";
 
-    var $prosc = h.to$(container),     // visible area (proscenium)
-        $stage = $(template),          // stage
-        $slides = toObject(slides);    // all slides
+    var $stage = h.to$(container),     // visible area
+        $slides = toObject(slides),    // all slides
+        $clones = $slides.clone(true); // all clones
 
     // options
     var opts = $.extend({
@@ -49,69 +42,36 @@ module.exports.transform = function (container, slides, options) {
     }, options);
 
     // values
-    var numSlides = $slides.length;
-    var scrollable = numSlides > opts.showNum;
-    var currId = opts.showNum;
+    var currId = 0;
 
     // Setup slider
-    if (scrollable) {
-        var i;
-        for (i = numSlides - 1; i >= numSlides - opts.showNum; i--) {
-            $stage.prepend($slides.eq(i).clone(true).data("slider-id", i));
-        }
-        for (i = 0; i < numSlides; i++) {
-            $stage.append($slides.eq(i).clone(true).data("slider-id", i));
-        }
-        for (i = 0; i < opts.showNum; i++) {
-            $stage.append($slides.eq(i).clone(true).data("slider-id", i));
-        }
-    } else {
-        for (i = 0; i < numSlides; i++) {
-            $stage.append($slides.eq(i).clone(true).data("slider-id", i));
-        }
-    }
-    $prosc.html($stage);
-
-    var $clones = $stage.children();
-    var numClones = $clones.length;
-    var stageWidth = getWidth($clones);
-
-    // stage init
-    //$stage.css("width", stageWidth).css("left", $clones.eq(currId).position().left * -1);
-    $stage.css("width", stageWidth).css("transform", "translateX(" + $clones.eq(currId).position().left * -1 + "px)");
-
+    $stage.append($clones);
 
     // set listeners
-    $prosc.on("nextSlides", function () {
-        currId += opts.scrollNum;
-        var currItem = $clones.eq(currId);
+    $stage.on("nextSlides", function () {
+        var nextId = currId + 1;
 
-        //$stage.css({
-        //    transition: transition,
-        //    left: currItem.position().left * -1
-        //});
+        var nextSlide = $clones.eq(nextId);
+        var currSlide = $clones.eq(currId);
 
-        $stage.css({
-            transition: transition,
-            transform: "translateX(" + currItem.position().left * -1 + "px)"
+        var width = getWidth(nextSlide);
+
+        // prepare
+        nextSlide.css("right", width * -1).addClass("ready");
+
+        // move
+        currSlide.css(anim.transform, "translateX(-" + width + "px)");
+        nextSlide.css(anim.transform, "translateX(-" + width + "px)");
+
+        // when done
+        currId = nextId;
+
+        // after transition finish
+        anim.tranFinish(true, nextSlide, function () {
+            nextSlide.addClass("active").removeClass("ready");
+            currSlide.removeClass("active");
+            nextSlide.css(anim.transform, "").css("right", "");
+            currSlide.css(anim.transform, "");
         });
-
-        if (numClones - currId < 2 * opts.showNum) {
-            $prosc.one("transitionend", function () {
-                var sliderId = currItem.data("slider-id");
-                currItem = $clones.filter(function () {
-                    return $(this).data("slider-id") === sliderId;
-                });
-                currId = currItem.index();
-                //$stage.css({
-                //    transition: "",
-                //    left: currItem.position().left * -1
-                //});
-                $stage.css({
-                    transition: "",
-                    transform: "translateX(" + currItem.position().left * -1 + "px)"
-                });
-            });
-        }
     });
 };

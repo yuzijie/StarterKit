383ef6fe95e583567b7d6a6f3edcd07866d44533
Zac YU <git@yuz.me>
2015-08-30 22:38:27 +0200

    update validator

--- js/validator.js
+++ js/validator.js
@@ -1,78 +1,55 @@
-// helper function
-function to$(item) {
-    return (item instanceof jQuery) ? item : $(item);
-}
+var h = require("./helper");
 
-//// Input check //////
-
-var checkInput = function ($input) {
-    var content = $input.val().trim(),
-        type = $input.prop("type"),
-        errorMessage;
-
-    if ($input.prop("required")) {
-        errorMessage = checkInputRequire($input, content);
-        if (errorMessage) return errorMessage;
-    }
-
-    if (content && type in checkers) {
-        errorMessage = checkers[type]($input, content);
-        if (errorMessage) return errorMessage;
-    }
-
-    return "";
-};
-
-// input type check
 var checkers = {
-    email: function ($input, content) {
+    email: function (content) {
         // Validate Email http://stackoverflow.com/questions/46155/validate-email-address-in-javascript
         var re = /^(([^<>()[\]\.,;:\s@"]+(\.[^<>()[\]\.,;:\s@"]+)*)|(".+"))@(([^<>()[\]\.,;:\s@"]+\.)+[^<>()[\]\.,;:\s@"]{2,})$/i;
         var result = re.test(content);
-        if (!result) return "您的邮箱格式不正确";
-        return null;
+        if (!result) return "email";
     }
 };
 
-// Check required field
-var checkInputRequire = function ($input, content) {
-    if (content.length > 0) return null;
-    return "required";
+//////// Input check ///////
+var checkInput = function ($input) {
+    var content = $input.val().trim(), type = $input.prop("type"), msg;
+
+    // check require
+    if ($input.prop("required") && !content.length) return "require";
+
+    // check format
+    if (content.length && (type in checkers) && (msg = checkers[type](content))) return msg;
+
+    // no error
+    return "ok";
 };
 
-
 //////// Group check ///////
-
 var checkGroup = function ($field) {
-    var checked = null,
-        min, max;
+    var checked = null, min, max;
 
     if ($field.data("required") === true) {
         if (checked === null) checked = $field.find(":checked").length;
-        if (checked === 0) return "required";
+        if (checked === 0) return "require";
     }
+
     if (min = $field.data("min-check")) {
         if (checked === null) checked = $field.find(":checked").length;
-        if (checked < min) return "您的选择不得小于" + min + "个";
+        if (checked < min) return "min";
     }
+
     if (max = $field.data("max-check")) {
         if (checked === null) checked = $field.find(":checked").length;
-        if (checked > max) return "您的选择不得大于" + max + "个";
+        if (checked > max) return "max";
     }
 
-    return "";
+    return "ok";
 };
 
-var Validator = {};
-
-Validator.check = function (field) {
-    var $field = to$(field);
-
-    if ($field.is("[data-input-group]")) {
-        return checkGroup($field);
-    } else {
-        return checkInput($field);
-    }
+//////// Modules ///////
+var check = function (field) {
+    var $field = h.to$(field);
+    if ($field.is("[data-input-group]")) return checkGroup($field);
+    return checkInput($field);
 };
 
-module.exports = Validator;
+module.exports = check;

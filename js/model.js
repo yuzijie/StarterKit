05fc0aab8af6bf85b1bd402dda7f22e704e1223c
Zac YU <git@yuz.me>
2015-09-12 00:15:15 +0200

    update

--- js/model.js
+++ js/model.js
@@ -1,3 +1,5 @@
+var XHR = require("./xhr");
+
 var Ev = function (sender) {
     this.sender = sender;
     this.listeners = [];
@@ -18,3 +20,163 @@ Ev.prototype = {
 
 };
 
+// for updating Model data
+function updateData(data, model) {
+    var i, keys = [], _data = model.data;
+
+    if (data instanceof Object) {
+        for (i in data) {
+            if (data.hasOwnProperty(i)) {
+
+                if (_data.hasOwnProperty(i)) {
+                    if (_data[i] !== data[i]) {
+                        _data[i] = data[i];
+                        keys.push(i);
+                    }
+                } else {
+                    _data[i] = data[i];
+                    keys.push(i);
+                }
+            }
+        }
+    } else {
+
+        var key = ++model.id + "";
+        _data[key] = data;
+        keys.push(key);
+    }
+
+    return keys;
+}
+
+var Model = function (data) {
+    this.id = 0;
+    this.data = data || {};
+
+    // events
+    this.updateEvent = new Ev(this);
+    this.deleteEvent = new Ev(this);
+    this.syncStartEvent = new Ev(this);
+    this.syncFinishEvent = new Ev(this);
+    this.syncFailEvent = new Ev(this);
+
+    // Ajax
+    this.request = new XHR();
+    this.syncKeys = []; // data to sync
+};
+
+Model.prototype = {
+
+    get: function (keys) {
+        if (keys || keys === 0) {
+            var i, key, output = {};
+
+            if (keys.constructor === Array) {
+                i = keys.length;
+                while (i--) {
+                    key = keys[i];
+                    output[key] = this.data[i];
+                }
+                return output;
+            }
+            return this.data[keys];
+        }
+        return this.data;
+    },
+
+    update: function (data) {
+        var i, keys = updateData(data, this);
+        if (i = keys.length) {
+            while (i--) {
+                if (this.syncKeys.indexOf(keys[i]) === -1) this.syncKeys.push(keys[i]);
+            }
+            this.updateEvent.notify({keys: keys});
+        }
+    },
+
+    delete: function (keys) {
+        var deleted = {};
+
+        if (keys || keys === 0) {
+            var i, key;
+
+            if (keys.constructor === Array) {
+                i = keys.length;
+
+                while (i--) {
+                    key = keys[i];
+                    if (this.data.hasOwnProperty(key)) {
+                        deleted[key] = this.data[key];
+                        delete this.data[key];
+                    }
+                }
+            } else {
+
+                if (this.data.hasOwnProperty(keys)) {
+                    deleted[keys] = this.data[keys];
+                    delete this.data[keys];
+                }
+            }
+        } else {
+
+            deleted = this.data;
+            this.data = {};
+        }
+
+        if (deleted) this.deleteEvent.notify({data: deleted});
+    },
+
+    sync: function (url, keys) {
+        var i, key, _this = this, data = {};
+
+        keys = [].concat(this.syncKeys, keys || []);
+
+        i = keys.length;
+        while (i--) {
+            key = keys[i];
+            if (this.data.hasOwnProperty(key)) data[key] = this.data[key];
+        }
+
+        if (data) {
+            this.syncStartEvent.notify({data: data});
+
+            if (url) this.xhr.updateUrl(url);
+            this.xhr.updateData(data);
+
+            this.xhr.send()
+                .done(function (data) {
+                    if (data.type !== "fail") _this.syncKeys = [];
+                    _this.syncFinishEvent.notify({response: data});
+                })
+                .fail(function () {
+                    _this.syncFailEvent.notify();
+                });
+        }
+    },
+
+    init: function (data) {
+        updateData(data, this);
+    },
+
+    listen: function (event, fn) {
+        switch (event) {
+            case "update":
+                this.updateEvent.attach(fn);
+                break;
+            case "delete":
+                this.deleteEvent.attach(fn);
+                break;
+            case "syncStart":
+                this.syncStartEvent.attach(fn);
+                break;
+            case "syncFinish":
+                this.syncFinishEvent.attach(fn);
+                break;
+            case "syncFail":
+                this.syncFailEvent.attach(fn);
+                break;
+        }
+    }
+};
+
+module.exports = Model;

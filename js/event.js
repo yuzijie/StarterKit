876a6360d3940644640ba2759475657df33bdff5
Zac YU <git@yuz.me>
2015-09-17 20:11:01 +0200

    update mvp event

---
+++ js/event.js
@@ -0,0 +1,53 @@
+var Event = function (model) {
+    this.model = model; // this event belongs to which model
+    this.listeners = []; // all listeners for this event
+    this.views = []; // records which view a listener belongs to
+};
+
+Event.prototype = {
+
+    attach: function (listener, viewId) {
+        this.listeners.push(listener);
+        this.views.push(viewId);
+    },
+
+    detach: function (obj) {                        // detach by listener or viewId
+        var id;
+
+        if (!obj) {                                 // if object is a falsy value
+
+            this.listeners = [];
+
+        } else if (obj instanceof Function) {       // if object is a listener
+
+            do {
+                id = this.listeners.indexOf(obj);
+                if (id !== -1) {
+                    this.listeners.splice(id, 1);
+                    this.views.splice(id, 1);
+                }
+            }
+            while (id !== -1);
+
+        } else {                                    // treat it as a viewId
+
+            id = this.views.length;
+            while (id--) {
+                if (this.views[id] == obj) {
+                    this.listeners.splice(id, 1);
+                    this.views.splice(id, 1);
+                }
+            }
+        }
+    },
+
+    notify: function (args) {
+        var l = this.listeners.length;
+        while (l--) {
+            this.listeners[l](args, this.model);
+        }
+    }
+
+};
+
+module.exports = Event;
